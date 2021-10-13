1cbea30c3ce99343f995ea2dccdac54936c9bf25
Tiago Martins <tiago.silva@flieger.software>
2021-10-13 12:33:18 +0200

    first commit

---
+++ src/components/PassiveIncome.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>
+        Meu nome é: {{ nome }}
+    </h1>
+</template>
+
+<script>
+export default {
+    name: 'PassiveIncome',
+    data(){
+        return {
+            nome: 'Ainda não sei'
+        }
+    },
+    created(){
+        this.nome = 'Tiago'
+    }
+}
+</script>
